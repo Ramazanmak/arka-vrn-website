ac8ed3d72fe5e6080265ca36cb2f43d65c11c0f2
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2025-03-16 10:36:27 +0300

    Some minor changes done!

--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -5,7 +5,7 @@ import ComHeader from '../common/ComHeader/ComHeader.vue';
 import HomeFirstScreen from '../home/HomeFirstScreen.vue';
 import About from '../home/About/About.vue';
 import Catalogue from '../home/Catalogue.vue';
-import Advantages from '../home/Advantages.vue';
+import Advantages from '../home/Advantages.vue'
 import ContactUs from '../home/ContactUs/ContactUs.vue';
 import ComFooter from '../common/ComFooter.vue';
 
