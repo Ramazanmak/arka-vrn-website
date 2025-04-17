1ff9f2aa6738d283abfa3e70d563366ce9c8f1ca
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2025-04-17 22:18:44 +0300

    Added closing panels by side click

--- src/components/views/CategoryPageView.vue
+++ src/components/views/CategoryPageView.vue
@@ -1,12 +1,24 @@
 <script setup>
+
+import {useRoute} from "vue-router";
+import { useTemplateRef } from 'vue';
+
 import ComHeader from '../common/ComHeader/ComHeader.vue';
 import HomeFirstScreen from '../home/HomeFirstScreen.vue';
 import ComFooter from '../common/ComFooter.vue';
 import CatPageTouchUs from '../categoryPage/CatPageTouchUs.vue';
 import ProductList from '../categoryPage/ProductList/ProductList.vue';
 import { catalogueFull } from '../../data/categories';
-import {useRoute} from "vue-router";
 
+const ComHeaderObj = useTemplateRef('header');
+function togglePanels(){
+    if(!ComHeaderObj.value.asideIsHidden){
+        ComHeaderObj.value.asideIsHidden = !ComHeaderObj.value.asideIsHidden;
+    } 
+    if(!ComHeaderObj.value.socialMediaListIsHidden){
+        ComHeaderObj.value.socialMediaListIsHidden = !ComHeaderObj.value.socialMediaListIsHidden
+    }
+}
 
 const route = useRoute();
 const data = catalogueFull.find(el => el.routeName == route.params.categoryName);
@@ -14,11 +26,11 @@ const data = catalogueFull.find(el => el.routeName == route.params.categoryName)
 </script>
 
 <template>
-    <ComHeader></ComHeader>
-    <HomeFirstScreen :pageName="data.name" :bgImage="data.bgImage"/>
-    <ProductList :category-object="data"/>
-    <CatPageTouchUs/> 
-    <ComFooter/>
+    <ComHeader ref="header"></ComHeader>
+    <HomeFirstScreen @click="togglePanels" :pageName="data.name" :bgImage="data.bgImage"/>
+    <ProductList @click="togglePanels" :category-object="data"/>
+    <CatPageTouchUs @click="togglePanels" /> 
+    <ComFooter @click="togglePanels" />
 </template>
 
 <style scoped>
